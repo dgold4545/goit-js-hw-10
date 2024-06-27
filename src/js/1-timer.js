78add4dbaff6f30918e5dd40a9acb03d35ad1622
DGOLD7788 <80936904545dgold@gmail.com>
2024-06-27 11:47:45 +0300

    add new code

---
+++ src/js/1-timer.js
@@ -0,0 +1,22 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+import { timerRefs } from './refs.js/timerRefs';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    timerRefs.buttonEl.removeAttribute('disabled');
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(timerRefs.inputEl, options);
+
+// timerRefs.buttonEl.setAttribute('disabled', true);
+// timerRefs.buttonEl.removeAttribute('disabled');
---
+++ src/js/refs.js/timerRefs.js
@@ -0,0 +1,5 @@
+export const timerRefs = {
+  inputEl: document.querySelector('input[type="text"]'),
+  buttonEl: document.querySelector('[data-start]'),
+  timerEl: document.querySelector('.timer'),
+};
