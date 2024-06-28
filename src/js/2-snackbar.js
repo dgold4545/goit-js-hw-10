9244a2072b9f10bb96e2642b35f6af8928adda74
DGOLD7788 <80936904545dgold@gmail.com>
2024-06-28 22:50:12 +0300

    add new code

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -23,14 +23,26 @@ function handlerFormSubmit(event) {
     snackbarPromise
       .then(delayValue => {
         iziToast.success({
+          title: 'OK',
           position: 'topCenter',
-          message: `✅ Fulfilled promise in ${delayValue}ms`,
+          message: `Fulfilled promise in ${delayValue}ms`,
+          backgroundColor: '#59A10D',
+          titleColor: 'white',
+          messageColor: 'white',
+          progressBarColor: '#326101',
+          iconUrl: '../img/ok.svg',
         });
       })
       .catch(delayValue => {
         iziToast.error({
+          title: 'Error',
           position: 'topCenter',
-          message: `❌ Rejected promise in ${delayValue}ms`,
+          message: `Rejected promise in ${delayValue}ms`,
+          titleColor: 'white',
+          messageColor: 'white',
+          backgroundColor: '#ef4040',
+          progressBarColor: '#b51b1b',
+          iconUrl: '../img/error.svg',
         });
       });
   }, delayValue);
